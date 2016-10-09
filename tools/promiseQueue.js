166ef7897b8b0dcd78e5708585e6e51637d191cc
ngtmuzi <ngtmuzi@qq.com>
2016-10-09 15:54:14 +0800

    promiseQueue:加入暂停与恢复功能

--- tools/promiseQueue.js
+++ tools/promiseQueue.js
@@ -9,9 +9,13 @@ var Promise = require('bluebird');
 function Queue(concurrency) {
   if (!(this instanceof Queue)) return new Queue(concurrency);
 
+  this.consumer = this._consumer.bind(this);
   this.queue    = [];
   this.runCount = 0;
   this.config(concurrency);
+  this.consumer   = this._consumer.bind(this);
+  this.pauseState = false;
+  this.wait       = Promise.resolve();
 }
 
 /**
@@ -20,11 +24,12 @@ function Queue(concurrency) {
  */
 Queue.prototype.add = function (fn) {
   var queue = this.queue;
-  setImmediate(this._consumer.bind(this));
+  setImmediate(this.consumer);
 
   return new Promise(function (resolve, reject) {
     queue.push(function () {
-      return Promise.try(fn).then(resolve, reject);
+      return Promise.try(fn)
+        .then(resolve, reject);
     });
   });
 };
@@ -35,9 +40,32 @@ Queue.prototype.add = function (fn) {
  */
 Queue.prototype.config = function (concurrency) {
   this.concurrency = +concurrency || 1;
-  setImmediate(this._consumer.bind(this));
+  for (var i = 0; i < this.concurrency; i++) {
+    setImmediate(this.consumer);
+  }
 };
 
+/**
+ * 暂停
+ */
+Queue.prototype.pause = function () {
+  var self        = this;
+  this.pauseState = true;
+  this.wait       = new Promise(function (resolve) {
+    self.waitForResume = resolve;
+  });
+};
+
+/**
+ * 继续
+ */
+Queue.prototype.resume = function () {
+  this.pauseState = false;
+  this.waitForResume && this.waitForResume();
+  for (var i = 0; i < this.concurrency; i++) {
+    setImmediate(this.consumer);
+  }
+};
 
 /**
  * 消费
@@ -46,13 +74,19 @@ Queue.prototype.config = function (concurrency) {
 Queue.prototype._consumer = function () {
   var self = this;
 
-  if (this.runCount >= this.concurrency || this.queue.length === 0) return;
+  if (this.runCount >= this.concurrency ||
+    this.queue.length === 0 ||
+    this.pauseState
+  ) return;
 
   this.runCount++;
   Promise.try(this.queue.shift())
-    .then(function () {
+    .tap(function () {
+      return self.wait;
+    })
+    .finally(function () {
       self.runCount--;
-      setImmediate(self._consumer.bind(self));
+      setImmediate(self.consumer);
     });
 };
 
